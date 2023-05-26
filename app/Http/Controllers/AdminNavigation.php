515a639d88f93a06a7b91b82db830a717c3c151e
gushinDev <gushindev@gmail.com>
2023-05-27 01:11:57 +0300

    Make simple navigation

--- app/Http/Controllers/AdminNavigation.php
+++ app/Http/Controllers/AdminNavigation.php
@@ -4,25 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\NavigationBar;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Cache;
 
 class AdminNavigation extends Controller
 {
-    protected Collection $navigationMenu;
-    public function __construct()
-    {
-        $this->navigationMenu = collect(Cache::get('navigation:bar'))->sortBy('order_number');
-    }
     public function index(): View
     {
-        return view('admin.index', [
-            'navigationMenu' => $this->navigationMenu
-        ]);
+        return view('admin.users.index');
     }
 
-    public function delete(Request $request)
+    public function delete(Request $request): RedirectResponse
     {
         NavigationBar::query()->where('navigation_bar_id', $request->get('menu_item'))->delete();
         return redirect()->to('navigation');
