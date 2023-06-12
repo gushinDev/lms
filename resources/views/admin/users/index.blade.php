024b7ed468e6b012f0cb53a4ad8219c7c043fe84
eu <gushindev@gmail.com>
2023-06-12 14:32:11 +0300

    Extract pomodoro timer into main navigation

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -71,21 +71,4 @@
         </table>
         {{$users->onEachSide(0)->links()}}
     </div>
-    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
-            aria-controls="offcanvasRight">Toggle right offcanvas
-    </button>
-
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasRightLabel">Pomodoro timer</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            <div><h2 id="timer">25:00</h2></div>
-            <button type="button" class="btn btn-success" onclick="startTimer()">Start</button>
-            <button type="button" class="btn btn-warning" onclick="stopTimer()">Stop</button>
-            <button type="button" class="btn btn-danger" onclick="resetTimer()">Reset</button>
-        </div>
-    </div>
-    @include('admin.users.scripts')
 @endsection
